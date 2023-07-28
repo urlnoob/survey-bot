da0fef83b3b9487940d4fd5fa1dd3a6ebf430811
smallerthan3 <89257093+urlnoob@users.noreply.github.com>
2023-07-28 17:28:13 +0200

    Update popup-neu.js

--- popup-neu.js
+++ popup-neu.js
@@ -1,17 +1,32 @@
-// Funktion zum Prüfen, ob der aktuelle Tag Freitag ist
-function isFriday() {
+// Funktion zum Prüfen, ob der aktuelle Tag Montag ist
+function isMonday() {
   const today = new Date();
-  return today.getDay() === 5; // Beachte: Sonntag ist 0, Montag ist 1, ... und Freitag ist 5
+  return today.getDay() === 5; // Montag ist 1 (Sonntag ist 0, Dienstag ist 2, ...), da getDay() Werte von 0 bis 6 zurückgibt.
 }
 
-// Funktion zum Laden des Typeform-Widgets, wenn heute Freitag ist
-function loadTypeformIfFriday() {
-  if (isFriday()) {
+// Funktion zum Laden des Typeform-Widgets, wenn heute Montag ist
+function loadTypeformIfMonday() {
+  if (isMonday()) {
+    const typeformContainer = document.createElement('div');
+    typeformContainer.setAttribute('data-tf-popover', 'Gw2xDIKY');
+    typeformContainer.setAttribute('data-tf-opacity', '100');
+    typeformContainer.setAttribute('data-tf-iframe-props', 'title=Status-Bericht');
+    typeformContainer.setAttribute('data-tf-open', 'time');
+    typeformContainer.setAttribute('data-tf-open-value', '60000');
+    typeformContainer.setAttribute('data-tf-auto-close', '604805000');
+    typeformContainer.setAttribute('data-tf-transitive-search-params', '');
+    typeformContainer.setAttribute('data-tf-button-color', '#000000');
+    typeformContainer.setAttribute('data-tf-notification-days', '7');
+    typeformContainer.setAttribute('data-tf-medium', 'snippet');
+    typeformContainer.setAttribute('style', 'all:unset;');
+
+    document.body.appendChild(typeformContainer);
+
     const script = document.createElement('script');
-    script.src = 'embed.typeform.com/next/embed.js';
+    script.src = '//embed.typeform.com/next/embed.js';
     document.head.appendChild(script);
   }
 }
 
 // Aufruf der Funktion beim Laden der Seite
-document.addEventListener('DOMContentLoaded', loadTypeformIfFriday);
+document.addEventListener('DOMContentLoaded', loadTypeformIfMonday);
